115c84d6c86d8b57566ec30cb5e0728917663b66
ghhhvbfbk-del <ghhhvbfbk@gmail.com>
2026-06-08 19:33:55 +0300

    Add files via upload

--- PyCharmMiscProject/script.py
+++ PyCharmMiscProject/script.py
@@ -21,14 +21,14 @@ while True:
     faces = face_cascade.detectMultiScale(
         gray,
         scaleFactor=1.1,  # Коэффициент уменьшения изображения на каждом масштабе
-        minNeighbors=5,  # Минимальное количество соседей для сохранения прямоугольника
-        minSize=(30, 30)  # Минимальный размер лица
+        minNeighbors=2,  # Минимальное количество соседей для сохранения прямоугольника
+        minSize=(31, 31)  # Минимальный размер лица
     )
 
     # Отрисовка прямоугольников вокруг обнаруженных лиц
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        cv2.putText(frame, 'Face', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (139, 0, 0), 1)
+        cv2.putText(frame,' maika',   (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (139, 0, 0), 2)
 
     # Отображение кадра с выделенными лицами
     cv2.imshow('Face Detection', frame)
